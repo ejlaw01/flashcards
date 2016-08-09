4485eb39edbad89fb0c198c99acfbae2067cb71c
Ethan Law and Sara Jensen <ethan@>
2016-08-09 16:36:00 -0700

    add rows and columns for flashcards, add borders, padding and margins to columns. add definitions that toggle on and off when you click the title.

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $("h3#javascript").click(function(){
+    $("p#defJavascript").toggle();
+  });
+  $("p#javascript").click(function(){
+    $("h3#defJavascript").toggle();
+  });
+
+  $("h3#operators").click(function(){
+    $("p#defOperators").toggle();
+  });
+  $("p#operators").click(function(){
+    $("h3#defOperators").toggle();
+  });
+
+  $("h3#variables").click(function(){
+    $("p#defVariables").toggle();
+  });
+  $("p#variables").click(function(){
+    $("h3#defVariables").toggle();
+  });
+
+  $("h3#varNameConventions").click(function(){
+    $("p#defVarNameConventions").toggle();
+  });
+  $("p#varNameConventions").click(function(){
+    $("h3#defVarNameConventions").toggle();
+  });
+
+  $("h3#functions").click(function(){
+    $("p#defFunctions").toggle();
+  });
+  $("p#functions").click(function(){
+    $("h3#defFunctions").toggle();
+  });
+
+  $("h3#methods").click(function(){
+    $("p#defMethods").toggle();
+  });
+  $("p#methods").click(function(){
+    $("h3#defMethods").toggle();
+  });
+
+
+
+
+
+});
